da6e73e89675cd49b76105093c08d721753d9796
Hao Yu <hao.yu.019@gmail.com>
2016-05-28 14:48:13 -0400

    Create postorderusingstack.cpp

---
+++ tree/postorderusingstack.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        stack<TreeNode *> s;
+        TreeNode *pre = NULL, *cur = root;
+        int SUM = 0;
+        while (cur || !s.empty()) {
+            while (cur) {
+                s.push(cur);
+                SUM += cur->val;
+                cur = cur->left;
+            }
+            cur = s.top();
+            if (cur->left == NULL && cur->right == NULL && SUM == sum) {
+                return true;
+            }
+            if (cur->right && pre != cur->right) {
+                cur = cur->right;
+            } else {
+                pre = cur;
+                s.pop();
+                SUM -= cur->val;
+                cur = NULL;
+            }
+        }
+        return false;
+    }
+};
