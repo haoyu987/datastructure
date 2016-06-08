ff53d048f2abd77f46a1444267be4aca3b14c4e3
Hao Yu <hao.yu.019@gmail.com>
2016-06-08 13:57:13 -0400

    Create Word Ladder.cpp

---
+++ hashtable/Word Ladder.cpp
@@ -0,0 +1,42 @@
+/*
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+1.Only one letter can be changed at a time
+2.Each intermediate word must exist in the word list
+
+*/
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
+        wordDict.insert(endWord);
+        queue<string> toVisit;
+        addNextWords(beginWord, wordDict, toVisit);
+        int dist = 2;
+        while (!toVisit.empty()) {
+            int num = toVisit.size();
+            for (int i = 0; i < num; i++) {
+                string word = toVisit.front();
+                toVisit.pop();
+                if (word == endWord) return dist;
+                addNextWords(word, wordDict, toVisit);
+            }
+            dist++;
+        }
+    }
+private:
+    void addNextWords(string word, unordered_set<string>& wordDict, queue<string>& toVisit) {
+        wordDict.erase(word);
+        for (int p = 0; p < (int)word.length(); p++) {
+            char letter = word[p];
+            for (int k = 0; k < 26; k++) { 
+                word[p] = 'a' + k;
+                if (wordDict.find(word) != wordDict.end()) {
+                    toVisit.push(word);
+                    wordDict.erase(word);
+                }
+            }
+            word[p] = letter;
+        } 
+    } 
+};
