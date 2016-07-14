d4b5993b5f9063ae100ba0d4ed441ddef98e0449
Hao Yu <hao.yu.019@gmail.com>
2016-07-14 15:31:05 -0400

    Update postorderusingstack.cpp

--- tree/postorderusingstack.cpp
+++ tree/postorderusingstack.cpp
@@ -26,3 +26,62 @@ public:
         return false;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution2 {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> treestack;
+        TreeNode *cur=root,*pre=NULL;
+        vector<int> res;
+        
+        while(!treestack.empty() || cur){
+            while(cur){
+                treestack.push(cur);
+                cur=cur->left;
+            }
+            cur=treestack.top();
+            if(cur->right && cur->right!=pre){
+                cur=cur->right;
+            }else{
+                res.push_back(cur->val);
+                pre = cur;
+                treestack.pop();
+                cur = NULL;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution3 {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        stack<TreeNode*> nodeStack;
+        vector<int> result;
+        //base case
+        if(root==NULL)
+        return result;
+        nodeStack.push(root);
+    while(!nodeStack.empty())
+    {
+        TreeNode* node= nodeStack.top();  
+        result.push_back(node->val);
+        nodeStack.pop();
+        if(node->left)
+        nodeStack.push(node->left);
+        if(node->right)
+        nodeStack.push(node->right);
+    }
+     reverse(result.begin(),result.end());
+     return result;
+    
+}
