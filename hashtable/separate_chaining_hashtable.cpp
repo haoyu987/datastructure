c34c5d95a134055c28be6ca6e3421e894e4315f4
Hao Yu <hao.yu.019@gmail.com>
2016-08-04 22:25:26 -0400

    Update separate_chaining_hashtable.cpp

--- hashtable/separate_chaining_hashtable.cpp
+++ hashtable/separate_chaining_hashtable.cpp
@@ -1,14 +1,66 @@
-template<typename HashObj>
+template<typename HashedObj>
 class HashTable
 {
   public:
     explicit HashTable(int size=101);
     
-    bool contains(const HashObj & x) const; // if HashObj is const.
+    bool contains(const HashedObj & x) const; // if HashObj is const.
     
     void makeEmpty();
-    bool insert(const HashObj & x);
-    bool insert(HashObj && x); //An rvalue reference behaves just like an lvalue reference except that it can bind to a temporary (an rvalue),
+    bool insert(const HashedObj & x);
+    bool insert(HashedObj && x); //An rvalue reference behaves just like an lvalue reference except that it can bind to a temporary (an rvalue),
                                //whereas you can not bind a (non const) lvalue reference to an rvalue.
+    bool remove(const HashedObj & x);
+    
+  private:
+    vector<list<HashedObj>> theLists;  // The array of lists.
+    int currentSize;
+    
+    void rehash();
+    size_t myhash(const HashedObj & x) const;
+};
 
+
+size_t myhash(const HashedObj & x) const
+{
+  static hash<HashedObj> hf;
+  return hf(x) % theLists.size();
+}
+
+void makeEmpty()
+{
+  for(auto & theList: theLists)
+    theList.clear();
+}
+
+bool contains(const HashedObj & x) const
+{
+  auto & whichList = theLists[myhash(x)];
+  return find(begin(whichList),end(whichList),x) != end(whichList);
+}
+
+bool remove(const HashedObj & x)
+{
+  auto & whichList = theLists[myhash(x)];
+  auto itr = find(begin(whichList),end(whichList),x);
+  
+  if(itr == end(whichList))
+    return false;
+    
+  whichList.erase(itr);
+  --currentSize;
+  return true;
+}
+
+bool insert(const HashedObj & x)
+{
+  auto & whichList = theLists[myhash(x)];
+  if(find(begin(whichList),end(whichList),x) != end(whichList))
+    return false;
+  whichList.push_back(x);
+  
+  if(++currentSize > theLists.size())
+    rehash;
+    
+  return true;
 }
