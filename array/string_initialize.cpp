4099474759ae744bcd575770adae55f7b63fefe4
Hao Yu <hao.yu.019@gmail.com>
2016-08-10 00:50:48 -0400

    Create string_initialize.cpp

---
+++ array/string_initialize.cpp
@@ -0,0 +1,26 @@
+// string constructor
+#include <iostream>
+#include <string>
+
+int main ()
+{
+  std::string s0 ("Initial string");
+  char arr[ ] = "This is a test";
+
+  string str(arr);
+
+  // constructors used in the same order as described above:
+  std::string s1;
+  std::string s2 (s0);
+  std::string s3 (s0, 8, 3);
+  std::string s4 ("A character sequence", 6);
+  std::string s5 ("Another character sequence");
+  std::string s6a (10, 'x');
+  std::string s6b (10, 42);      // 42 is the ASCII code for '*'
+  std::string s7 (s0.begin(), s0.begin()+7);
+
+  std::cout << "s1: " << s1 << "\ns2: " << s2 << "\ns3: " << s3;
+  std::cout << "\ns4: " << s4 << "\ns5: " << s5 << "\ns6a: " << s6a;
+  std::cout << "\ns6b: " << s6b << "\ns7: " << s7 << '\n';
+  return 0;
+}
